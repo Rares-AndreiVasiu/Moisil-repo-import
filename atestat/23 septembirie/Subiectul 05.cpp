7bf972167a3871875615e286b45b800b825ade6d
Vasiu Rareș <72668860+Rares-AndreiVasiu@users.noreply.github.com>
2021-11-28 20:19:00 +0200

    Create Subiectul 05.cpp

---
+++ atestat/23 septembirie/Subiectul 05.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <fstream>
+
+std::ifstream fin("numere.in");
+
+int sub(int x)
+{
+    if (x <= 1)
+        return 0;
+
+    if (x % 2 == 0 and x != 2)
+        return 0;
+
+    for (int i = 3; i * i <= x; i += 2)
+        if (x % i == 0)
+            return 0;
+
+    return 1;
+}
+
+int n, x = 2;
+
+int main()
+{
+    fin >> n;
+
+    while (n)
+    {
+        if (sub(x))
+        {
+            std::cout << x << ' ';
+
+            n--;
+        }
+        x++;
+    }
+
+    fin.close();
+
+    return 0;
+}
